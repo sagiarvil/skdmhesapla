f92682750540f5fe4f4bd81ffd209bca759811e7
sagi arvil <teb232@gmail.com>
2026-08-22 18:27:54 +0300

    feat(regulatory): register scheduled monitoring functions

---
+++ functions/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Keep the existing API untouched; extend the Firebase export surface with
+// isolated regulatory monitoring functions.
+const apiExports = require("./index.js");
+const regulatoryExports = require("./regulatory-monitor.js");
+
+module.exports = {
+  ...apiExports,
+  regulatoryWatch15m: regulatoryExports.regulatoryWatch15m,
+  regulatoryDigestDaily: regulatoryExports.regulatoryDigestDaily,
+};
