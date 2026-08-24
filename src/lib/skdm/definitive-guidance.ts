e88be84d724d614fcc1ee9fa008a650fd4e0b8be
sagi arvil <teb232@gmail.com>
2026-08-24 22:49:39 +0300

    feat(regulatory): version definitive-period guidance provenance

---
+++ src/lib/skdm/definitive-guidance.ts
@@ -0,0 +1,15 @@
+export const CBAM_DEFINITIVE_GUIDANCE = {
+  version: "2026-08-14.1",
+  publishedAt: "2026-08-14",
+  sourceUrl:
+    "https://taxation-customs.ec.europa.eu/carbon-border-adjustment-mechanism/cbam-legislation-and-guidance_en",
+  guidanceCount: 10,
+  calculationGuidance: "Guidance No. 3 - CBAM methods for the calculation of emissions embedded in goods",
+  freeAllocationGuidance:
+    "Guidance No. 4 - CBAM calculation of the free allocation adjustment to the number of CBAM certificates to be surrendered",
+  sectorGuides: ["cement", "hydrogen", "fertiliser", "iron-steel", "aluminium", "electricity"],
+  legalCalculationAuthority: "Commission Implementing Regulation (EU) 2025/2547",
+  legalFreeAllocationAuthority: "Commission Implementing Regulation (EU) 2025/2620",
+  boundary:
+    "14 Ağustos rehberleri uygulama açıklamasıdır; hesaplama ve free-allocation motorunda çelişki halinde bağlayıcı implementing acts esas alınır.",
+} as const;
