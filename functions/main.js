2fcbe1167bb678b499b0e3144f86822673610a3f
sagi arvil <teb232@gmail.com>
2026-09-05 11:32:38 +0300

    Wire maritime commerce function

--- functions/main.js
+++ functions/main.js
@@ -6,12 +6,14 @@ const cbamSealExports = require("./cbam-seal-v2.js");
 const regulatoryExports = require("./regulatory-monitor-email.js");
 const maritimeExports = require("./maritime-backend-v2.js");
 const maritimeEvidenceExports = require("./maritime-evidence-v3.js");
+const maritimeCommerceExports = require("./maritime-commerce-v1.js");
 
 module.exports = {
   ...apiExports,
   cbamApiV2: cbamSealExports.cbamApiV2,
   maritimeApi: maritimeExports.maritimeApi,
   maritimeEvidenceApi: maritimeEvidenceExports.maritimeEvidenceApi,
+  maritimeCommerceApi: maritimeCommerceExports.maritimeCommerceApi,
   regulatoryWatch15m: regulatoryExports.regulatoryWatch15m,
   regulatoryDigestDaily: regulatoryExports.regulatoryDigestDaily,
 };
