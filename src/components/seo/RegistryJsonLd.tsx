06700de208615c01f4fab117db02e7e1338efd46
sagi arvil <teb232@gmail.com>
2026-09-10 01:08:22 +0300

    fix: avoid duplicate EU importer schema entity

--- src/components/seo/RegistryJsonLd.tsx
+++ src/components/seo/RegistryJsonLd.tsx
@@ -9,9 +9,13 @@ const MD_ROUTES = new Set(
     .map((r) => r.route as string),
 );
 
+const SELF_MANAGED_JSON_LD = new Set(["/eu-importers/"]);
+
 export function RegistryJsonLd({ route }: { route: string }) {
   const data = jsonLdDocument(route);
   const markdown = MD_ROUTES.has(route);
+  const selfManagedJsonLd = SELF_MANAGED_JSON_LD.has(route);
+
   return (
     <>
       {markdown ? (
@@ -24,10 +28,12 @@ export function RegistryJsonLd({ route }: { route: string }) {
       {markdown ? (
         <link rel="describedby" href={`${SITE_ORIGIN}/llms.txt`} />
       ) : null}
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-      />
+      {selfManagedJsonLd ? null : (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+        />
+      )}
     </>
   );
 }
